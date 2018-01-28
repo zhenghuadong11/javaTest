fcd47bcce1f20caf2b486bfc44f6b1f079e2da64
apple <>
2018-01-28 17:32:18 +0800

    mybatis

--- mybatis/src/main/java/mybatis/MybatisTest.java
+++ mybatis/src/main/java/mybatis/MybatisTest.java
@@ -4,20 +4,24 @@ package mybatis;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
+import java.util.Map;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.log4j.BasicConfigurator;
 
 public class MybatisTest {
 
     public static void main(String[] args) throws IOException {
+    	BasicConfigurator.configure();
+    	
         //mybatis的配置文件
-        String resource = "conf.xml";
+        String resource = "config.xml";
         System.out.println("nihaoa");
         //使用类加载器加载mybatis的配置文件（它也加载关联的映射文件）
-        InputStream is = String.class.getClassLoader().getResourceAsStream(resource);
+        InputStream is = MybatisTest.class.getClassLoader().getResourceAsStream(resource);
         //构建sqlSession的工厂
         SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(is);
         //使用MyBatis提供的Resources类加载mybatis的配置文件（它也加载关联的映射文件）
@@ -33,7 +37,12 @@ public class MybatisTest {
          */
         String statement = "me.gacl.mapping.userMapper.getUser";//映射sql的标识字符串
         //执行查询返回一个唯一user对象的sql
-        User user = session.selectOne(statement, 1);
+        Map user = session.selectOne(statement, 1);
         System.out.println(user);
+        session.commit();
+        session.close();
+       
+        
+       
     }
 }
